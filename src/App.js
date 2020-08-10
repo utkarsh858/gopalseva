42ebcb90df4fa4514eec1ad49365fab3826adfb3
Aman <amanamals@gmail.com>
2020-08-10 13:50:05 +0530

    donation new

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Route
 } from "react-router-dom";
 
-import donation from "./donation.js"
+import Donation from "./Donation.js"
 import Home from "./Home.js"
 
 export default class App extends Component{
