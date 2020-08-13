14ca8c1201c4ab305b08615398d5709b08ff3d52
Aman <amanamals@gmail.com>
2020-08-13 14:07:45 +0530

    style donation

--- src/Donation2.js
+++ src/Donation2.js
@@ -71,48 +71,174 @@ var yyyy = today.getFullYear();
 
     return (
 
-        <form target="_blank" method="post" action="https://us-central1-gopal-seva.cloudfunctions.net/api/data" enctype='application/json'>
-        Name:
-        <input type="text" name="name" onChange = {(event) => {
-                this.setState({name: event.target.value});
-                }} /><br/>
-        email:
+            <div>
+                    <div class="header fixed-top">
+                            <nav class="navbar navbar-expand-lg navbar-light">
+                                    <div class="container">
+                                            <a class="navbar-brand" href="/"><img src="https://drive.google.com/file/d/1zrpkz0mi9xlHMKEGb6FU4HXpmQPfgxrg/view?usp=sharing" alt="" /></a>
+                                    </div>
+                            </nav>
+                    </div>
+                    <div class="container">
+
+                            
+
+        <form class="needs-validation" target="_blank" method="post" action="https://us-central1-gopal-seva.cloudfunctions.net/api/data" enctype='application/json' novalidate>
+        
+                <div class="form-row">
+                        <div class="col-md-4 mb-3">
+                                <label for="validationCustom01">Name</label>
+                                        <input style={{ backgroundColor: "whitesmoke" }} type="text" class="form-control" name="name" placeholder="Name"
+                                                            required onChange={(event) => {
+                                                                    this.setState({ name: event.target.value });
+                                                            }} /><br />
+                                                <div class="valid-feedback">
+                                                        Looks good!
+      </div>
+                                            </div>
+                        
+                                            <div class="col-md-4 mb-3">
+                                                    <label for="validationCustomUsername">Email</label>
+                                                    <div class="input-group">
+                                                            <div class="input-group-prepend">
+                                                                    <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                                            </div>
+
+                                                            <input style={{ backgroundColor: "whitesmoke" }} type="email" class="form-control" name="email" placeholder="Email" aria-describedby="inputGroupPrepend" required onChange={(event) => {
+                                                                    this.setState({ email: event.target.value });
+                                                            }} /><br />
+
+                                                            <div class="invalid-feedback">
+                                                                    Please choose an email.
+        </div>
+                                                    </div>
+                                            </div>
+                                    </div>
+                                                            
+        {/* email:
         <input type="text" name="email" onChange = {(event) => {
                 this.setState({email: event.target.value});
-                }} /><br/>
-        address:
+                }} /><br/> */}
+
+                                    <div class="form-row">
+                                            <div class="form-group col-md-8">
+                                                    <label for="inputAddress">Address</label>
+                                                    <input style={{ backgroundColor: "whitesmoke" }} type="text" class="form-control" placeholder="1234 Main St" name="address" onChange={(event) => {
+                                                            this.setState({ address: event.target.value });
+                                                    }} /><br />
+                                            </div>
+                                            
+
+                                    </div>
+
+
+                                    <div class="form-row">
+                                            <div class="form-group col-md-4">
+                                                    <label for="validationCustom03">City</label>
+                                                    <input style={{ backgroundColor: "whitesmoke" }} type="text" class="form-control" placeholder="City" required name="city" onChange={(event) => {
+                                                            this.setState({ city: event.target.value });
+                                                    }} /><br />
+                                                    <div class="invalid-feedback">
+                                                            Please provide a valid city.
+      </div>
+                                            </div>
+                                            <div class="col-md-4 mb-4">
+                                                    <label for="validationCustom04">State</label>
+                                                    <input style={{ backgroundColor: "whitesmoke" }} type="text" class="form-control" placeholder="State" required name="state" onChange={(event) => {
+                                                            this.setState({ state: event.target.value });
+                                                    }} /><br />
+                                                    <div class="invalid-feedback">
+                                                            Please provide a valid state.
+      </div>
+                                            </div>
+
+                                           
+
+                                    </div>    
+
+                                    <div class="form-row">
+
+                                            <div class="col-md-4 mb-4">
+                                                    <label for="validationCustom05">Country</label>
+                                                    <input style={{ backgroundColor: "whitesmoke" }} type="text" class="form-control" placeholder="Country" required name="country" onChange={(event) => {
+                                                            this.setState({ country: event.target.value });
+                                                    }} /><br />
+                                                    <div class="invalid-feedback">
+                                                            Please provide a valid Country.
+      </div>
+                                            </div>
+
+                                    <div class="col-md-4 mb-4">
+                                            <label for="validationCustom05">PIN</label>
+                                            <input style={{ backgroundColor: "whitesmoke" }} type="text" class="form-control" placeholder="PIN" required name="pin" onChange={(event) => {
+                                                    this.setState({ pin: event.target.value });
+                                            }} /><br />
+                                            <div class="invalid-feedback">
+                                                    Please provide a valid pin.
+      </div>
+                                    </div>
+
+                                    
+                        </div>
+
+                                    <div class="form-row">
+                                            <div class="form-group col-md-4">
+                                                    <label for="validationCustom06">PAN</label>
+                                                    <input style={{ backgroundColor: "whitesmoke" }} type="text" class="form-control" placeholder="PAN" required name="pan" onChange={(event) => {
+                                                            this.setState({ pan: event.target.value });
+                                                    }} /><br />
+                                                    <div class="invalid-feedback">
+                                                            Please provide a valid Pan.
+                                </div>
+                                            </div>
+
+                                            <div class="form-group col-md-4">
+                                                    <label for="validationCustom07">Mobile</label>
+                                                    <input style={{ backgroundColor: "whitesmoke" }} type="text" class="form-control" placeholder="Mobile" required name="mobile" onChange={(event) => {
+                                                            this.setState({ mobile: event.target.value });
+                                                    }} /><br />
+                                                    <div class="invalid-feedback">
+                                                            Please provide a valid Mobile Number.
+                                </div>
+                                            </div>
+                                    </div>
+
+        {/* address:
         <input type="text" name="address" onChange = {(event) => {
                 this.setState({address: event.target.value});
-                }} /><br/>
-        city:
+                }} /><br/> */}
+        {/* city:
         <input type="text" name="city" onChange = {(event) => {
                 this.setState({city: event.target.value});
-                }} /><br/>
-        state:
+                }} /><br/> */}
+        {/* state:
         <input type="text" name="state" onChange = {(event) => {
                 this.setState({state: event.target.value});
-                }} /><br/>
-        country:
+                }} /><br/> */}
+        {/* country:
         <input type="text" name="country" onChange = {(event) => {
                 this.setState({country: event.target.value});
-                }} /><br/>
-        pin:
+                }} /><br/> */}
+        {/* pin:
         <input type="text" name="pin" onChange = {(event) => {
                 this.setState({pin: event.target.value});
-                }} /><br/>
-        pan:
+                }} /><br/> */}
+        {/* pan:
         <input type="text" name="pan" onChange = {(event) => {
                 this.setState({pan: event.target.value});
                 }} /><br/>
         mobile:
         <input type="text" name="mobile" onChange = {(event) => {
                 this.setState({mobile: event.target.value});
-                }} /><br/>
+                }} /><br/> */}
         <input type="text" name="link" value={link} />
         <input type="text" name="date" value={date} />
         <input type="text" name="time" value={time} />
-        <input type="submit" onClick={()=>{window.location.href=link}}/>
+                <input class="btn btn-primary btn-md align-right" type="submit" onClick={()=>{window.location.href=link}} value="Proceed" />
         </form>
+
+        </div>
+        </div>
         );}
 }
 
