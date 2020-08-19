386977bcd19ffb0be54b882943de692748a22cab
mohiiitr <mkumar2@cs.iitr.ac.in>
2020-08-19 15:31:27 +0530

    slider control & responsive creation of carousel

--- src/Home.js
+++ src/Home.js
@@ -196,21 +196,25 @@ scrollTo = (target) =>{
 	    // the naming can be any, depends on you.
 	    breakpoint: { max: 4000, min: 3000 },
 	    items: 5,
+        slidesToSlide : 3,
         partialVisibilityGutter: 40
 	  },
 	  desktop: {
 	    breakpoint: { max: 3000, min: 1024 },
 	    items: 3,
+        slidesToSlide : 3, 
         partialVisibilityGutter: 10
 	  },
 	  tablet: {
 	    breakpoint: { max: 1024, min: 464 },
 	    items: 2,
+        slidesToSlide : 2,
         partialVisibilityGutter: 30
 	  },
 	  mobile: {
 	    breakpoint: { max: 464, min: 0 },
 	    items: 1,
+        slidesToSlide : 1,
         partialVisibilityGutter: 30
 	  }
 	};
