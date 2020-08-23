f8d4b918bb304045fee5fff3dedb8776f7587dae
utkarsh <utkarsh858iitr@gmail.com>
2020-08-23 14:53:03 +0530

    added cloud ideas file for future animated backgrounds

---
+++ src/cloud_ideas.js
@@ -0,0 +1,116 @@
+
+<div id="Clouds" style={{zIndex:56}}>
+  <div class="Cloud Foreground"></div>
+  <div class="Cloud Background"></div>
+  <div class="Cloud Foreground"></div>
+  <div class="Cloud Background"></div>  <div class="Cloud Foreground"></div>
+  <div class="Cloud Background"></div>  <div class="Cloud Foreground"></div>
+  <div class="Cloud Background"></div>  <div class="Cloud Foreground"></div>
+  <div class="Cloud Background"></div>  <div class="Cloud Foreground"></div>
+  <div class="Cloud Background"></div>  
+</div>
+
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
+   width="40px" height="24px" viewBox="0 0 40 24" enable- >
+  <defs>
+    <path id="Cloud" d="M33.85,14.388c-0.176,0-0.343,0.034-0.513,0.054c0.184-0.587,0.279-1.208,0.279-1.853c0-3.463-2.809-6.271-6.272-6.271
+  c-0.38,0-0.752,0.039-1.113,0.104C24.874,2.677,21.293,0,17.083,0c-5.379,0-9.739,4.361-9.739,9.738
+  c0,0.418,0.035,0.826,0.084,1.229c-0.375-0.069-0.761-0.11-1.155-0.11C2.811,10.856,0,13.665,0,17.126
+  c0,3.467,2.811,6.275,6.272,6.275c0.214,0,27.156,0.109,27.577,0.109c2.519,0,4.56-2.043,4.56-4.562
+  C38.409,16.43,36.368,14.388,33.85,14.388z"/>
+  </defs>
+</svg>
+
+<div id="rev_slider_314_1_wrapper" class="rev_slider_wrapper fullscreen-container" data-alias="mountain-parallax-header" data-source="gallery" style={{background:"rgba(255,255,255,0)",padding:"0px"}}>
+                        <div id="rev_slider_314_1" class="rev_slider fullscreenbanner" style={{display:"none"}} data-version="5.4.3.1">
+                            <ul>
+                                <li data-index="rs-896" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-rotate="0" data-saveperformance="off" data-title="Slide"
+                                    data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+
+
+                                    <div class="tp-caption   tp-resizeme  blurslider-gradient rs-parallaxlevel-14" id="slide-896-layer-2" data-x="['center','center','center','center']" data-hoffset="['1','1','1','1']" data-y="['middle','middle','middle','middle']" data-voffset="['-229','-229','-189','-149']"
+                                        data-fontsize="['100','100','100','80']" data-lineheight="['100','100','100','60']" data-letterspacing="['0','0','0','0']" data-width="none" data-height="none" data-whitespace="nowrap" data-type="text" data-responsive_offset="on"
+                                        data-frames='[{"delay":50,"speed":3000,"frame":"0","from":"y:50px;sX:0.3;sY:0.3;opacity:0;fb:20px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+                                        data-textAlign="['center','center','center','center']" data-paddingtop="[20,20,20,20]" data-paddingright="[10,10,10,10]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[10,10,10,10]" style={{"z-index": "5", "white-space": "nowrap", "font-size": "150px", "line-height": "120px", "font-weight": "700", color: "#ffffff", "font-family":"Roboto"}}>GITA OnLINE </div>
+
+                                   
+                                </li>
+                            </ul>
+                            <div class="tp-bannertimer tp-bottom" style={{visibility: "hidden !important"}}></div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                     <div class="tp-caption resize  tp-resizeme" id="slide-896-layer-1" data-x="['center','center','center','center']" data-hoffset="['-50','-50','-50','-50']" data-y="['middle','middle','middle','middle']" data-voffset="['130','130','130','80']" data-width="none"
+                                        data-height="none" data-whitespace="nowrap" data-type="image" data-responsive_offset="on" data-frames='[{"delay":50,"speed":3000,"frame":"0","from":"sX:0.7;sY:0.7;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+                                        data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 6;">
+                                        <img src="images/logo/banner.png" alt="" data-ww="['1050px','1050px','861','689px']" data-hh="['610px','610px','500px','400px']" width="500" height="610" class="banner-img-size" data-no-retina/>
+                                    </div>
+
+                                    <div class="tp-caption tp-shape tp-shapewrapper  tp-resizeme" id="slide-896-layer-10" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['0','0','0','0']" data-width="full"
+                                        data-height="200" data-whitespace="nowrap" data-type="shape" data-basealign="slide" data-responsive_offset="on" data-frames='[{"delay":50,"speed":1000,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                                        data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 7;background:linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);">
+                                    </div>
+
+                                    <div class="tp-caption   tp-resizeme" id="slide-896-layer-4" data-x="['left','left','left','left']" data-hoffset="['100','100','100','100']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['10','10','10','10']" data-width="none" data-height="none"
+                                        data-whitespace="nowrap" data-type="image" data-basealign="slide" data-responsive_offset="on" data-frames='[{"delay":300,"speed":2000,"frame":"0","from":"y:100px;sX:1.1;sY:1.1;opacity:0;fb:10px;","to":"o:1;rY:180;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+                                        data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 8;">
+                                        <div class="rs-looped rs-slideloop" data-easing="Linear.easeNone" data-speed="40" data-xs="0" data-xe="500" data-ys="0" data-ye="0"><img src="http://karate.w3itexperts.com/xhtml/images/slider/cloud1.png" alt="" data-ww="['500px','500px','500px','500px']" data-hh="['273px','273px','273px','273px']" width="500" height="273" data-no-retina/> </div>
+                                    </div>
+
+                                    <div class="tp-caption   tp-resizeme" id="slide-896-layer-5" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['10','10','10','10']" data-width="none" data-height="none"
+                                        data-whitespace="nowrap" data-type="image" data-basealign="slide" data-responsive_offset="on" data-frames='[{"delay":300,"speed":2000,"frame":"0","from":"sX:1.1;sY:1.1;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+                                        data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 9;">
+                                        <div class="rs-looped rs-slideloop" data-easing="Linear.easeNone" data-speed="100" data-xs="0" data-xe="500" data-ys="0" data-ye="0"><img src="http://karate.w3itexperts.com/xhtml/images/slider/cloud2.png" alt="" data-ww="['500px','500px','500px','500px']" data-hh="['273px','273px','273px','273px']" width="600" height="278" data-no-retina/> </div>
+                                    </div>
+
+                                    <div class="tp-caption   tp-resizeme" id="slide-896-layer-6" data-x="['right','right','right','right']" data-hoffset="['150','150','150','150']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['10','10','10','10']" data-width="none" data-height="none"
+                                        data-whitespace="nowrap" data-type="image" data-basealign="slide" data-responsive_offset="on" data-frames='[{"delay":300,"speed":2000,"frame":"0","from":"sX:1.1;sY:1.1;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+                                        data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 10;">
+                                        <div class="rs-looped rs-slideloop" data-easing="Linear.easeNone" data-speed="120" data-xs="0" data-xe="500" data-ys="0" data-ye="0"><img src="http://karate.w3itexperts.com/xhtml/images/slider/cloud3.png" alt="" data-ww="['500px','500px','500px','500px']" data-hh="['273px','273px','273px','273px']" width="738" height="445" data-no-retina/> </div>
+                                    </div>
+
+                                    <div class="tp-caption   tp-resizeme" id="slide-896-layer-11" data-x="['left','left','left','left']" data-hoffset="['-200','-200','-200','-200']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['10','10','10','10']" data-width="none" data-height="none"
+                                        data-whitespace="nowrap" data-type="image" data-basealign="slide" data-responsive_offset="on" data-frames='[{"delay":300,"speed":2000,"frame":"0","from":"sX:1.1;sY:1.1;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+                                        data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 11;">
+                                        <div class="rs-looped rs-slideloop" data-easing="Linear.easeNone" data-speed="80" data-xs="0" data-xe="500" data-ys="0" data-ye="0"><img src="http://karate.w3itexperts.com/xhtml/images/slider/cloud3.png" alt="" data-ww="['500px','500px','500px','500px']" data-hh="['273px','273px','273px','273px']" width="738" height="445" data-no-retina/> </div>
+                                    </div>
+
+                                    <div class="tp-caption   tp-resizeme" id="slide-896-layer-12" data-x="['right','right','right','right']" data-hoffset="['-200','-200','-200','-200']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['10','10','10','10']" data-width="none"
+                                        data-height="none" data-whitespace="nowrap" data-type="image" data-basealign="slide" data-responsive_offset="on" data-frames='[{"delay":300,"speed":2000,"frame":"0","from":"y:100px;sX:1.1;sY:1.1;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+                                        data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 12;">
+                                        <div class="rs-looped rs-slideloop" data-easing="Linear.easeNone" data-speed="200" data-xs="0" data-xe="500" data-ys="0" data-ye="0"><img src="http://karate.w3itexperts.com/xhtml/images/slider/cloud1.png" alt="" data-ww="['500px','500px','500px','500px']" data-hh="['273px','273px','273px','273px']" width="500" height="273" data-no-retina/> </div>
+                                    </div>
+
+                                    <div class="tp-caption " id="slide-896-layer-3" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['100','100','100','150']" data-width="none" data-height="none"
+                                        data-whitespace="nowrap" data-type="button" data-actions='[{"event":"click","action":"scrollbelow","offset":"px","delay":"","speed":"1500","ease":"Power1.easeInOut"}]' data-responsive_offset="on" data-responsive="off"
+                                        data-frames='[{"delay":500,"speed":1000,"frame":"0","from":"y:50px;sX:1;sY:1;opacity:0;fb:20px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"},{"frame":"hover","speed":"200","ease":"Linear.easeNone","to":"o:1;rX:0;rY:0;rZ:0;z:0;fb:0;","style":"c:rgba(255,255,255,1);bg:linear-gradient(90deg, rgba(243,130,60,1) 0%, rgba(255,85,124,1) 100%);bs:solid;bw:0 0 0 0;"}]'
+                                        data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 13; font-weight: 700;">
+                                        <div class="btn btn-primary">READ MORE</div>
+                                    </div>
+
+                                    <div class="tp-caption   tp-resizeme" id="slide-896-layer-8" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['bottom','bottom','bottom','bottom']" data-voffset="['50','30','30','30']" data-width="['none','640','480','360']"
+                                        data-height="none" data-whitespace="['nowrap','normal','normal','normal']" data-type="text" data-responsive_offset="on" data-frames='[{"delay":600,"speed":1000,"frame":"0","from":"y:50px;rX:90deg;opacity:0;fb:10px;","to":"o:1;fb:0;","ease":"Power4.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
+                                        data-textAlign="['center','center','center','center']" data-paddingtop="[0,0,0,0]" data-paddingright="[10,10,10,10]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[20,20,20,20]" style="z-index: 14; white-space: nowrap; font-size: 15px; line-height: 20px; font-weight: 600; color: #000; letter-spacing: 10px;font-family:Roboto;text-transform:uppercase;">Bhagavad Gita on Life, Interests, Nature & Emotions....</div>
